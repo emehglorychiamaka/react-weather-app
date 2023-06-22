601f59c6673e3a32074b83f518da08b4604ca1f3
emehglorychiamaka <emehglorychiamaka@gmail.com>
2023-06-22 12:17:51 +0100

    added github repo URL

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">Weather App</header>
+      <footer>
+        created by{" "}
+        <a
+          href="https://www.linkedin.com/in/emeh-glory-chiamaka-9586b0aa"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Emeh Glory Chiamaka
+        </a>{" "}
+        <a
+          href="https://github.com/emehglorychiamaka/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
